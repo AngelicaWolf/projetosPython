7baa5629b2dbbd7133bbf6813861245f59655fc4
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-10 16:28:16 -0300

    feat/leaguageUpdate
    The language was changed to English

--- exemplo.py
+++ exemplo.py
@@ -1,29 +1,26 @@
-def calcular_cedulas_moedas(valor):
-    cedulas = [200, 100, 50, 20, 10, 5, 2]
-    moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
+def calculate_bills_coins(value):
+    bills = [200, 100, 50, 20, 10, 5, 2]
+    coins = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
     
-    resultado = {}
+    result = {}
     
-    # Calcular cédulas
-    for cedula in cedulas:
-        if valor >= cedula:
-            quantidade = valor // cedula
-            valor -= quantidade * cedula
-            resultado[f"Cédula de R${cedula}"] = int(quantidade)
+    for bill in bills:
+        if value >= bill:
+            quantity = value // bill
+            value -= quantity * bill
+            result[f"CÃ©dula de R${bills}"] = int(quantity)
     
-    # Calcular moedas
-    for moeda in moedas:
-        if valor >= moeda:
-            quantidade = valor // moeda
-            valor -= quantidade * moeda
-            resultado[f"Moeda de R${moeda:.2f}"] = int(quantidade)
+    for coin in coins:
+        if value >= coin:
+            quantity = value // coin
+            value -= quantity * coin
+            result[f"Coin de R${coin:.2f}"] = int(quantity)
     
-    return resultado
+    return result
 
-# Exemplo de uso
-valor = float(input("Digite a quantia em reais: "))
-resultado = calcular_cedulas_moedas(valor)
+value = float(input("Digite a quantia em reais: "))
+result = calculate_bills_coins(value)
 
-print("Para compor a quantia de R${:.2f}, você precisa de:".format(valor))
-for item, quantidade in resultado.items():
-    print(f"{quantidade} x {item}")
+print("Para compor a quantia de R${:.2f}, vocÃª precisa de:".format(value))
+for item, quantity in result.items():
+    print(f"{quantity} x {item}")

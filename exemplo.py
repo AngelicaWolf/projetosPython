d3b4f5595a944abc6b0dedcb82abe073884a19ef
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-14 14:13:16 -0300

    feat/addInventoryManagementSystem

--- exemplo.py
+++ exemplo.py
@@ -1,29 +1,41 @@
-def calcular_cedulas_moedas(valor):
-    cedulas = [200, 100, 50, 20, 10, 5, 2]
-    moedas = [1, 0.50, 0.25, 0.10, 0.05, 0.01]
-    
-    resultado = {}
-    
-    # Calcular c�dulas
-    for cedula in cedulas:
-        if valor >= cedula:
-            quantidade = valor // cedula
-            valor -= quantidade * cedula
-            resultado[f"C�dula de R${cedula}"] = int(quantidade)
-    
-    # Calcular moedas
-    for moeda in moedas:
-        if valor >= moeda:
-            quantidade = valor // moeda
-            valor -= quantidade * moeda
-            resultado[f"Moeda de R${moeda:.2f}"] = int(quantidade)
-    
-    return resultado
+estoque = {
+    "tomate": [50, 2.5],  
+    "batata": [30, 1.8], 
+    "alface": [20, 1.2],  
+}
+venda = []
+def imprimir_estoque():
+    print("Estoque:\n")
+    for chave, dados in estoque.items():
+        print("Descrição:", chave)
+        print("Quantidade:", dados[0])
+        print("Preço: %.2f\n" % dados[1])
+def realizar_venda():
+    total = 0
+    print("\nVendas:\n")
+    produto_vendido = input("Digite o nome do produto vendido: ").lower()
+    if produto_vendido not in estoque:
+        print("Produto inválido! O produto não existe no estoque.")
+        return
+    try:
+        quantidade_vendida = int(input(f"Quantos {produto_vendido} foram vendidos? "))
+    except ValueError:
+        print("Quantidade inválida! Por favor, digite um número inteiro.")
+        return
+    if quantidade_vendida > estoque[produto_vendido][0]:
+        print(f"Estoque insuficiente para {produto_vendido}. Apenas {estoque[produto_vendido][0]} unidades disponíveis.")
+        return
+    preço = estoque[produto_vendido][1]
+    custo = preço * quantidade_vendida
+    print(f"{produto_vendido.capitalize():12s}: {quantidade_vendida:3d} x {preço:6.2f} = {custo:6.2f}")
+    estoque[produto_vendido][0] -= quantidade_vendida
+    total += custo
+    print(f"Custo total: {total:21.2f}\n")
+while True:
+    # Realizar uma venda
+    realizar_venda()
+    continuar = input("Deseja realizar outra venda? (s/n): ").lower()
+    if continuar != 's':
+        break
+imprimir_estoque()
 
-# Exemplo de uso
-valor = float(input("Digite a quantia em reais: "))
-resultado = calcular_cedulas_moedas(valor)
-
-print("Para compor a quantia de R${:.2f}, voc� precisa de:".format(valor))
-for item, quantidade in resultado.items():
-    print(f"{quantidade} x {item}")

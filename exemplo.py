efdd68f37d9b6645025e0478c9b6ca49d05894e4
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-14 13:39:45 -0300

    feat/leaguageUpdate
    Language changed to English

--- exemplo.py
+++ exemplo.py
@@ -1,26 +1,26 @@
 import numpy as np
-def eh_identidade(matriz):
-    identidade = np.eye(matriz.shape[0], dtype=int)  
-    return np.array_equal(matriz, identidade)
-def ler_matriz(n):
-    matriz = []
+def eh_identity(matrix):
+    identity = np.eye(matrix.shape[0], dtype=int)  
+    return np.array_equal(matrix, identity)
+def ler_matrix(n):
+    matrix = []
     for i in range(n):
         linha = []
         for j in range(n):
             valor = int(input(f"Digite o valor para a posição [{i}][{j}] da matriz: "))
             linha.append(valor)
         matriz.append(linha)
-    return np.array(matriz)
+    return np.array(matrix)
 try:
     n = int(input("Digite o tamanho da matriz quadrada (ex: 3 para 3x3): "))
     if n <= 0:
         raise ValueError("O tamanho da matriz deve ser positivo.")
     print("\nDigite os valores da matriz A:")
-    A = ler_matriz(n)
+    A = ler_matrix(n)
     print("\nDigite os valores da matriz B:")
-    B = ler_matriz(n)
+    B = ler_matrix(n)
     AB = np.dot(A, B)
-    if eh_identidade(AB):
+    if eh_identity(AB):
         print("\nAs matrizes A e B são inversas, pois A * B é a matriz identidade.")
     else:
         print("\nAs matrizes A e B não são inversas, pois A * B não é a matriz identidade.")

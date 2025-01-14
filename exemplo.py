4cee8bd50a7386211dbf1929692a04cbe00cc823
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-14 14:58:38 -0300

    feat/lenguageUpdate
    Language changed to English

--- exemplo.py
+++ exemplo.py
@@ -1,41 +1,40 @@
-estoque = {
-    "tomate": [50, 2.5],  
-    "batata": [30, 1.8], 
-    "alface": [20, 1.2],  
+stock = {
+    "tomato": [50, 2.5],  
+    "potato": [30, 1.8], 
+    "lettuce": [20, 1.2],  
 }
-venda = []
-def imprimir_estoque():
+sale = []
+def print_stock():
     print("Estoque:\n")
-    for chave, dados in estoque.items():
-        print("Descrição:", chave)
-        print("Quantidade:", dados[0])
-        print("Preço: %.2f\n" % dados[1])
-def realizar_venda():
+    for key, data in stock.items():
+        print("Descrição:", key)
+        print("Quantidade:", data[0])
+        print("Preço: %.2f\n" % data[1])
+def make_sale():
     total = 0
     print("\nVendas:\n")
-    produto_vendido = input("Digite o nome do produto vendido: ").lower()
-    if produto_vendido not in estoque:
+    product_sold = input("Digite o nome do produto vendido: ").lower()
+    if product_sold not in stock:
         print("Produto inválido! O produto não existe no estoque.")
         return
     try:
-        quantidade_vendida = int(input(f"Quantos {produto_vendido} foram vendidos? "))
+        quantity_sold = int(input(f"Quantos {product_sold} foram vendidos? "))
     except ValueError:
         print("Quantidade inválida! Por favor, digite um número inteiro.")
         return
-    if quantidade_vendida > estoque[produto_vendido][0]:
-        print(f"Estoque insuficiente para {produto_vendido}. Apenas {estoque[produto_vendido][0]} unidades disponíveis.")
+    if quantity_sold > stock[product_sold][0]:
+        print(f"Estoque insuficiente para {product_sold}. Apenas {stock[product_sold][0]} unidades disponíveis.")
         return
-    preço = estoque[produto_vendido][1]
-    custo = preço * quantidade_vendida
-    print(f"{produto_vendido.capitalize():12s}: {quantidade_vendida:3d} x {preço:6.2f} = {custo:6.2f}")
-    estoque[produto_vendido][0] -= quantidade_vendida
-    total += custo
+    price = stock[product_sold][1]
+    cost = price * quantity_sold
+    print(f"{product_sold.capitalize():12s}: {quantity_sold:3d} x {price:6.2f} = {cost:6.2f}")
+    stock[product_sold][0] -= quantity_sold
+    total += cost
     print(f"Custo total: {total:21.2f}\n")
 while True:
-    # Realizar uma venda
-    realizar_venda()
-    continuar = input("Deseja realizar outra venda? (s/n): ").lower()
-    if continuar != 's':
+ 
+    make_sale()
+    continue = input("Deseja realizar outra venda? (s/n): ").lower()
+    if continue != 's':
         break
-imprimir_estoque()
-
+print_stock()

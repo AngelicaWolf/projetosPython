99f3d4e4786e4cad018fa416c109d7f9b5c6da80
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-15 14:09:44 -0300

    feat/lenguageUpdate

--- exemplo.py
+++ exemplo.py
@@ -1,18 +1,18 @@
-PC= {"Lamborghini Aventador":1500000.00,"Dodge Viper": 1100000.00,"Porsche Cayenne": 200000.00,"Ford Mustang": 350000.00,"Ferrari Enzo":12000000.00}
-for carro, preco in PC.items():
-    print(f"{carro}: R$ {preco:,.2f}")
+pc= {"Lamborghini Aventador":1500000.00,"Dodge Viper": 1100000.00,"Porsche Cayenne": 200000.00,"Ford Mustang": 350000.00,"Ferrari Enzo":12000000.00}
+for car, price in pc.items():
+    print(f"{car}: R$ {price:,.2f}")
 while True:
-    carro_input = input("Digite o nome do carro ou 'fim' para sair: ")
-    if carro_input.lower() == 'fim':
+    car_input = input("Digite o nome do carro ou 'fim' para sair: ")
+    if car_input.lower() == 'fim':
         break
-    if carro_input in PC:
-        print(f"Preço de {carro_input}: R$ {PC[carro_input]:,.2f}")
+    if car_input in pc:
+        print(f"Preço de {car_input}: R$ {pc[car_input]:,.2f}")
     else:
         print("Carro não encontrado. Tente novamente.")
 print("\nTodos os carros e seus preços:")
-for carro, preco in PC.items():
-    print(f"{carro}: R$ {preco:,.2f}")
-print("Porsche Cayenne" in PC) 
-print("Tesla Model S" in PC)  
-print("Chaves (nomes dos carros):", PC.keys())
-print("Valores (preços):", PC.values())
+for car, price in pc.items():
+    print(f"{car}: R$ {price:,.2f}")
+print("Porsche Cayenne" in pc) 
+print("Tesla Model S" in pc)  
+print("Chaves (nomes dos carros):", pc.keys())
+print("Valores (preços):", pc.values())

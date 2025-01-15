2a8f100380cdf60eae79dcdd95c2b7fd61f0f077
Angélica <angelica.wolf.ifpr@gmail.com>
2025-01-15 13:54:40 -0300

    feat/lenguageUpdate

--- exemplo.py
+++ exemplo.py
@@ -1,10 +1,10 @@
 pc={"Processador":1500,"Placa-mãe":500,"Memória-RAM":600,"Placa-de-Vídeo":1200}
 while True:
-    componente=input("Digite o nome do componente ou 'fim' para sair:")
-    if componente=="fim":
+    component=input("Digite o nome do componente ou 'fim' para sair:")
+    if component=="fim":
      break
-    if componente in pc:
-        print(f"Preço{pc[componente]:5.2f}")
+    if component in pc:
+        print(f"Preço{pc[component]:5.2f}")
     else:
         print("Produto não encontrado")
 del pc["Placa-de-Vídeo"]
